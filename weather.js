7782cc3139f5cdb3310cd67939f354cdab5b75af
Adarsh M M <adarshakashmm@gmail.com>
2024-05-12 07:53:40 +0530

    styling and responsive

--- weather.js
+++ weather.js
@@ -2,8 +2,12 @@
     let APIkey='6af6bdc540c3b760cfa832214a8b4a5b';
     let lat;
     let lon;
+    document.querySelector(".main").style.display="block"
+    document.querySelector(".last").style.display="flex";
+    document.querySelector(".error").textContent="";
     async function getApi(){
-    let fetched= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=15&appid=${APIkey}`);
+    try{
+     let fetched= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=15&appid=${APIkey}`);
     let data=await fetched.json();
     lat=await data[0].lat.toFixed(2)
     lon=await data[0].lon.toFixed(2)
@@ -16,17 +20,23 @@
     const wind=respond.wind.speed;
     console.log(respond);
     return {temp,humid,condition,wind,icon};
-    
+    }
+    catch(err){
+        document.querySelector(".main").style.display="none"
+        document.querySelector(".last").style.display="none";
+        document.querySelector(".error").textContent="Something went wrong";
+    }
     }
 async function getWeather(){
     let weatherData=await getApi();
-    document.querySelector(".name").textContent=cityName;
+    document.querySelector(".name").textContent=cityName.toUpperCase();
     document.querySelector(".temp").textContent=`${weatherData.temp}°C`;
     document.querySelector(".condition").textContent=weatherData.condition;
     document.querySelector(".h").textContent=`${weatherData.humid} %`;
     document.querySelector(".w").textContent=`${weatherData.wind} m/s`;
 }
 document.querySelector(".adding").addEventListener("click",()=>{
+    document.querySelector(".text").value="";
     document.querySelector("form").style.display="block";
     document.querySelector(".adding").style.display="none";
 })
@@ -37,5 +47,5 @@ document.querySelector(".btn").addEventListener("click",()=>{
  document.querySelector(".adding").style.display="inline";
  document.querySelector("form").style.display="none";
  getWeather()
-    }
+    }else{  document.querySelector(".error").textContent="Something went wrong";}
 });getWeather();
